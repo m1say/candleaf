8c4e3c3760392ae26389ef63df61d7942c16639b
Mark Cobo <misay@misay-2.local>
2021-10-26 21:03:57 +0800

    Set sticky navbar to mobile only

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,71 +25,76 @@ const Navbar = () => {
     }
   };
   return (
-    <header ref={header} className={cn(styles["header"], "inner-container")}>
-      <div ref={overlay} className={cn(styles["overlay"], "fade-animation")}>
-        <Link href="/#products">
-          <a onClick={toggleHamburger}>Shop</a>
-        </Link>
-        <Link href="#">
-          <a onClick={toggleHamburger}>About</a>
-        </Link>
-        <Link href="#">
-          <a onClick={toggleHamburger}>Contact Us</a>
-        </Link>
-      </div>
-      <nav className={cn(styles["nav"], "flex flex-jc-sb flex-ai-c")}>
-        <a
-          className={cn(styles["header__toggle"], "hide-for-desktop")}
-          onClick={toggleHamburger}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </a>
-        <Link href="/">
-          <a className={cn(styles["header__logo"], "flex flex-ai-c")}>
-            <Image
-              priority
-              src="/logo.png"
-              height={34}
-              width={126}
-              alt="candleaf-logo"
-            />
-          </a>
-        </Link>
-
-        <div className={cn(styles["header__links"], "hide-for-mobile")}>
+    <header ref={header} className={cn(styles["header"])}>
+      <div className="inner-container">
+        <div ref={overlay} className={cn(styles["overlay"], "fade-animation")}>
           <Link href="/#products">
-            <a>Shop</a>
+            <a onClick={toggleHamburger}>Shop</a>
           </Link>
           <Link href="#">
-            <a>About</a>
+            <a onClick={toggleHamburger}>About</a>
           </Link>
           <Link href="#">
-            <a>Contact Us</a>
+            <a onClick={toggleHamburger}>Contact Us</a>
           </Link>
         </div>
-
-        <div className={cn(styles["header__actions"])}>
-          <Link href="/cart">
-            <a
-              suppressHydrationWarning={true}
-              className={cn(styles["header__cart"], "flex flex-ai-c flex-jc-c")}
-            >
+        <nav className={cn(styles["nav"], "flex flex-jc-sb flex-ai-c")}>
+          <a
+            className={cn(styles["header__toggle"], "hide-for-desktop")}
+            onClick={toggleHamburger}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+          <Link href="/">
+            <a className={cn(styles["header__logo"], "flex flex-ai-c")}>
               <Image
                 priority
-                src="/icons/icon-cart.svg"
-                height={23}
-                width={23}
-                layout="fixed"
-                alt="shopping-cart"
+                src="/logo.png"
+                height={34}
+                width={126}
+                alt="candleaf-logo"
               />
-
-              {cartCount !== 0 && <span>{cartCount}</span>}
             </a>
           </Link>
-        </div>
-      </nav>
+
+          <div className={cn(styles["header__links"], "hide-for-mobile")}>
+            <Link href="/#products">
+              <a>Shop</a>
+            </Link>
+            <Link href="#">
+              <a>About</a>
+            </Link>
+            <Link href="#">
+              <a>Contact Us</a>
+            </Link>
+          </div>
+
+          <div className={cn(styles["header__actions"])}>
+            <Link href="/cart">
+              <a
+                suppressHydrationWarning={true}
+                className={cn(
+                  styles["header__cart"],
+                  "flex flex-ai-c flex-jc-c"
+                )}
+              >
+                <Image
+                  priority
+                  src="/icons/icon-cart.svg"
+                  height={23}
+                  width={23}
+                  layout="fixed"
+                  alt="shopping-cart"
+                />
+
+                {cartCount !== 0 && <span>{cartCount}</span>}
+              </a>
+            </Link>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
